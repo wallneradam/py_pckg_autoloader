63dd02ae93b5d01887d9ff5c0c9e05fd8385a6b9
wallneradam <adam.wallner@gmail.com>
2019-11-18 14:42:28 +0100

    Much cleaner and simpler

--- module_loader.py
+++ module_loader.py
@@ -1,6 +1,6 @@
 import sys
 import pkgutil
-from importlib.util import module_from_spec
+from importlib import import_module
 
 
 # noinspection PyProtectedMember
@@ -9,7 +9,8 @@ def load_all_modules():
     Load all modules in the directory of the caller
     """
     # Get the context of the caller module
-    caller_globals = list(sys._current_frames().values())[-1].f_back.f_globals
+    caller_globals = sys._getframe(1).f_globals
+    caller_name = caller_globals['__name__']
 
     # Create the __all__ magic if not exists
     if '__all__' not in caller_globals:
@@ -19,18 +20,10 @@ def load_all_modules():
     for info in pkgutil.walk_packages(caller_globals['__path__']):
         if info.ispkg:  # Skip packages
             continue
-        module_path = caller_globals['__name__'] + '.' + info.name
+
+        module_path = caller_name + '.' + info.name
         if module_path not in sys.modules:
-            # Get module specification
-            spec = info.module_finder.find_spec(info.name)
-            # Load module without execution
-            _module = module_from_spec(spec)
-            # Set the parent package to be able to relative import
-            _module.__package__ = caller_globals['__package__']
-            # Register it into sys.modules
-            sys.modules[module_path] = _module
-            # Execute the module
-            spec.loader.exec_module(_module)
+            import_module('.' + info.name, caller_name)
 
         # Add the module to the globals of the caller module
         caller_globals[info.name] = sys.modules[module_path]
